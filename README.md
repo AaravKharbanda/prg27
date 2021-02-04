46d18e7dfd9b594d77b71aacfa1118b68cb91f75
AaravKharbanda <73995741+AaravKharbanda@users.noreply.github.com>
2021-02-04 17:15:16 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# prg27
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ bob.js
@@ -0,0 +1,25 @@
+class bob
+{
+    constructor(x,y)
+    {
+        var options = {
+            isStatic: false,
+            restitution: 0.3,
+            friction: 0.5,
+            density: 1.2
+        }
+        this.body = Bodies.circle(x,y,50,options);
+        World.add(world, this.body);
+       
+    }
+
+    display(){
+       push()
+       translate(this.body.position.x, this.body.position.y)
+       rotate(this.body.angle)
+       rectMode(CENTER)           
+       circle(0, 0, this.width, this.height);       
+       pop() 
+       
+    }
+}
